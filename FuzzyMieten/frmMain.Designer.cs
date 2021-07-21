f318b7505d856505ac6e9ee46a999a20b5e96780
Kelothras <renejan.schiffner@gmail.com>
2021-07-21 12:31:04 +0200

    Namensänderung Korrektur #1

--- FuzzyMieten/frmMain.Designer.cs
+++ FuzzyMieten/frmMain.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace FuzzyMieten
+namespace FuzzyInput
 {
     partial class frmMain
     {
--- FuzzyMieten/frmMain.cs
+++ FuzzyMieten/frmMain.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.VisualBasic;
 
-namespace FuzzyMieten
+namespace FuzzyInput
 {
     public partial class frmMain : Form
     {
